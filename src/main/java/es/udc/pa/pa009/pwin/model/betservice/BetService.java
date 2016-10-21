74a77e280b6718d7618284d76a506937707df645
daniel <daniel.ferreiro@udc.es>
2016-10-21 13:37:01 +0200

    Corregida la posibilidad de apostar con una cantidad negativa. Closes #3
    Corregida la posibilidad de apostar con una cantidad negativa, y creada
    la excepción y tests correspondientes.

--- src/main/java/es/udc/pa/pa009/pwin/model/betservice/BetService.java
+++ src/main/java/es/udc/pa/pa009/pwin/model/betservice/BetService.java
@@ -7,7 +7,6 @@ import es.udc.pa.pa009.pwin.model.apuesta.Opcion;
 import es.udc.pa.pa009.pwin.model.evento.Categoria;
 import es.udc.pa.pa009.pwin.model.evento.Evento;
 import es.udc.pa.pa009.pwin.model.evento.ExpiredEventException;
-import es.udc.pa.pa009.pwin.model.userprofile.UserProfile;
 import es.udc.pojo.modelutil.exceptions.InstanceNotFoundException;
 
 public interface BetService {
@@ -21,16 +20,16 @@ public interface BetService {
 			int startIndex, int count);
 
 	public Apuesta bet(Long idOpcion, Double cantidadApostada, Long userId)
-			throws ExpiredEventException, InstanceNotFoundException;
+			throws ExpiredEventException, InstanceNotFoundException, NegativeAmountException;
 
 	public ApuestaBlock checkBet(Long userId, int startIndex, int count);
 
 	public Evento findEventById(Long idEvento) throws InstanceNotFoundException;
-	
+
 	public int getNumberOfEvents(String keywords, Long categoryId);
-	
+
 	public int getNumberOfBets(Long userID);
-	
+
 	public List<Opcion> showWinners (Long idTipoApuesta);
 
 	public List<Evento> findAllEvents(Long idCategoria, String keywords);
--- src/main/java/es/udc/pa/pa009/pwin/web/pages/betPages/Bet.java
+++ src/main/java/es/udc/pa/pa009/pwin/web/pages/betPages/Bet.java
@@ -7,7 +7,6 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 import org.apache.tapestry5.annotations.Component;
-import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.corelib.components.Form;
@@ -18,6 +17,7 @@ import es.udc.pa.pa009.pwin.model.apuesta.Apuesta;
 import es.udc.pa.pa009.pwin.model.apuesta.Opcion;
 import es.udc.pa.pa009.pwin.model.apuesta.TipoApuesta;
 import es.udc.pa.pa009.pwin.model.betservice.BetService;
+import es.udc.pa.pa009.pwin.model.betservice.NegativeAmountException;
 import es.udc.pa.pa009.pwin.model.evento.Evento;
 import es.udc.pa.pa009.pwin.model.evento.ExpiredEventException;
 import es.udc.pa.pa009.pwin.web.pages.adminPages.SuccessfulCreation;
@@ -35,96 +35,98 @@ public class Bet {
 
 	@Component
 	private Form betForm;
-	
+
 	@SessionState(create=false)
 	private UserSession userSession;
-	
+
 	@Property
 	private Double cantidad;
-	
+
 	@Inject
 	private BetService betService;
-	
+
 	@Inject
 	private Messages messages;
-	
+
 	@Inject
 	private Locale locale;
-	
+
 	private Long idOpcion;
 	private Apuesta apuestaRealizada;
-	
+
 	@Property
 	private Evento evento;
 	@Property
 	private TipoApuesta tipoApuesta;
 	@Property
 	private Opcion opcion;
-	
-	
-	
+
+
+
 	void onActivate(Long idOpcion){
-		
-		
+
+
 		this.idOpcion=idOpcion;
-		
-			try {
-				opcion=betService.findOptionById(idOpcion);
-			} catch (InstanceNotFoundException e) {
-				betForm.recordError( messages.get("error-optionNotFound") );
-				
-			} catch (ExpiredEventException e) {
-				betForm.recordError( messages.get("error-expiredEvent") );				
-				
-			}
-		
+
+		try {
+			opcion=betService.findOptionById(idOpcion);
+		} catch (InstanceNotFoundException e) {
+			betForm.recordError( messages.get("error-optionNotFound") );
+
+		} catch (ExpiredEventException e) {
+			betForm.recordError( messages.get("error-expiredEvent") );				
+
+		}
+
 		tipoApuesta=opcion.getTipoApuesta();
 		evento=tipoApuesta.getEvento();
 	}
-	
+
 	Long onPassivate() {
 		return idOpcion;
 	}
-	
-	
-	
-	
+
+
+
+
 	public void setIdOpcion(Long id) {
 		idOpcion=id;
 	}
-	
+
 	public Long getIdOpcion() {
 		return idOpcion;
 	}
-	
+
 	public NumberFormat getNumberFormat() {
 		return NumberFormat.getInstance(locale);
 	}
-	
-	
+
+
 	void onValidateFromBetForm(){
 		Long userId = userSession.getUserProfileId();
-		
+
 		try {
 			apuestaRealizada = betService.bet(idOpcion, cantidad, userId);
 		} catch (ExpiredEventException e) {
 			betForm.recordError( messages.get("error-expiredEvent")  );
-			
+
 		} catch (InstanceNotFoundException e) {
 			betForm.recordError( messages.get("error-optionNotFound"));
 
+		} catch (NegativeAmountException e) {
+			betForm.recordError(messages.get("error-optionNotDound"));
 		}
-		
+
 	}
-	
+
 	Object onSuccess() {
-		
+
 		return SuccessfulCreation.class;
-		
+
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 }

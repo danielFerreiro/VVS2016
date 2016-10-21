187f108a63f1f3199720793aa3d0f88622813308
daniel <daniel.ferreiro@udc.es>
2016-10-21 12:42:19 +0200

    Realización de los Tests de los DAO's.
    Realización de los Tests de los DAO's y modificación del gitignore, asi
    como el traslado de la Memoria al directorio doc

--- src/test/java/es/udc/pa/pa009/pwin/test/vvs/VVSTest.java
+++ src/test/java/es/udc/pa/pa009/pwin/test/vvs/VVSTest.java
@@ -3,6 +3,7 @@ package es.udc.pa.pa009.pwin.test.vvs;
 import static es.udc.pa.pa009.pwin.model.util.GlobalNames.SPRING_CONFIG_FILE;
 import static es.udc.pa.pa009.pwin.test.util.GlobalNames.SPRING_CONFIG_TEST_FILE;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -269,6 +270,10 @@ public class VVSTest {
 
 
 
+	/**
+	 * Este test se debería ejecutar siempre uno de los primeros, puesto que los
+	 * demás dependen de el.
+	 */
 	@Test
 	public void testPR_UN_AD_01() {
 		// initialize
@@ -280,7 +285,7 @@ public class VVSTest {
 
 		// test
 		Apuesta bet = new Apuesta(Calendar.getInstance(), user, 20.0, option);
-
+		apuestaDao.save(bet);
 	}
 
 
@@ -309,8 +314,8 @@ public class VVSTest {
 		Opcion option = insertOptionWithoutState(betType);
 		Apuesta bet = insertApuesta(user, option);
 
+		// test
 		Apuesta betFound = apuestaDao.find(bet.getIdApuesta());
-
 		assertEquals(betFound, bet);
 	}
 
@@ -334,8 +339,23 @@ public class VVSTest {
 		apuestaDao.remove(new Long(-1));
 	}
 
-	@Test
-	public void testPR_UN_AD_08() {
+	@Test(expected = InstanceNotFoundException.class)
+	public void testPR_UN_AD_08() throws InstanceNotFoundException {
+
+		// initialize
+		UserProfile user = insertUser("usuario1");
+		Categoria category = insertCategory();
+		Evento event = insertEvent(category);
+		TipoApuesta betType = insertBetTypeWithoutMultipleWinnerOptions(event);
+		Opcion option = insertOptionWithoutState(betType);
+
+		// test
+		Apuesta bet = new Apuesta(Calendar.getInstance(), user, 20.0, option);
+
+		apuestaDao.save(bet);
+		apuestaDao.find(bet.getIdApuesta());
+		apuestaDao.remove(bet.getIdApuesta());
+		apuestaDao.find(bet.getIdApuesta());
 
 	}
 
@@ -436,72 +456,275 @@ public class VVSTest {
 	@Test
 	public void testPR_UN_AD_11() {
 
+		// initialize
+		UserProfile user = insertUser("usuario1");
+		UserProfile user2 = insertUser("usuario2");
+
+		Categoria category = insertCategory();
+		Evento event = insertEvent(category);
+		Evento event2 = insertEvent(category);
+
+		TipoApuesta betType10 = insertBetTypeWithoutMultipleWinnerOptions(event);
+		TipoApuesta betType20 = insertBetTypeWithoutMultipleWinnerOptions(event2);
+
+		Opcion option10 = insertOptionWithoutState(betType10);
+		Opcion option11 = insertOptionWithoutState(betType10);
+		Opcion option12 = insertOptionWithoutState(betType10);
+		Opcion option20 = insertOptionWithoutState(betType20);
+		Opcion option21 = insertOptionWithoutState(betType20);
+
+		Apuesta bet1 = insertApuesta(user, option11);
+		Apuesta bet2 = insertApuesta(user2, option12);
+		Apuesta bet3 = insertApuesta(user, option20);
+		Apuesta bet4 = insertApuesta(user, option10);
+
+		bet1.getFecha().add(Calendar.SECOND, 2);
+		bet3.getFecha().add(Calendar.SECOND, 5);
+		bet4.getFecha().add(Calendar.SECOND, 8);
+
+		// test
+		int numberOfBets = apuestaDao.findNumberOfBets(user.getUserProfileId());
+
+		assertEquals(3, numberOfBets);
 	}
 
 	@Test
 	public void testPR_UN_AD_12() {
 
+		// initialize
+		UserProfile user = insertUser("usuario1");
+		UserProfile user2 = insertUser("usuario2");
+
+		// test
+		int numberOfBets = apuestaDao.findNumberOfBets(user.getUserProfileId());
+
+		assertEquals(0, numberOfBets);
+
 	}
 
 	@Test
 	public void testPR_UN_OD_01() {
+		// initialize
+		UserProfile user = insertUser("usuario1");
+		UserProfile user2 = insertUser("usuario2");
+
+		Categoria category = insertCategory();
+		Evento event = insertEvent(category);
+		Evento event2 = insertEvent(category);
+
+		TipoApuesta betType10 = insertBetTypeWithoutMultipleWinnerOptions(event);
+		TipoApuesta betType20 = insertBetTypeWithoutMultipleWinnerOptions(event2);
+
+		Opcion option11 = insertOptionWithoutState(betType10);
+		Opcion option12 = insertOptionWithoutState(betType10);
+		Opcion option13 = insertLoserOption(betType10);
+		Opcion option21 = insertWinnerOption(betType20);
+		Opcion option22 = insertLoserOption(betType20);
+
+		// test
+		List<Opcion> winnerOption = opcionDao.showWinners(betType10.getIdTipo());
+
+		assertTrue(winnerOption.isEmpty());
 
 	}
 
 	@Test
 	public void testPR_UN_OD_02() {
+		// initialize
+		UserProfile user = insertUser("usuario1");
+		UserProfile user2 = insertUser("usuario2");
 
+		Categoria category = insertCategory();
+		Evento event = insertEvent(category);
+		Evento event2 = insertEvent(category);
+
+		TipoApuesta betType10 = insertBetTypeWithoutMultipleWinnerOptions(event);
+		TipoApuesta betType20 = insertBetTypeWithoutMultipleWinnerOptions(event2);
+
+		Opcion option11 = insertWinnerOption(betType10);
+		Opcion option12 = insertWinnerOption(betType10);
+		Opcion option13 = insertLoserOption(betType10);
+		Opcion option21 = insertWinnerOption(betType20);
+		Opcion option22 = insertLoserOption(betType20);
+
+		// test
+		List<Opcion> winnerOptions = opcionDao.showWinners(betType10.getIdTipo());
+
+		assertEquals(2, winnerOptions.size());
+		assertTrue(winnerOptions.contains(option11));
+		assertTrue(winnerOptions.contains(option12));
 	}
 
 	@Test
 	public void testPR_UN_TAD_01() {
 
+		Categoria category = insertCategory();
+		Evento event = insertEvent(category);
+		TipoApuesta betType = insertBetTypeWithoutMultipleWinnerOptions(event);
+
+		boolean exists = tipoApuestaDao.findDuplicateBetTypes(betType.getPregunta(), event.getIdEvento());
+
+		assertTrue(exists);
 	}
 
 	@Test
 	public void testPR_UN_CD_01() {
+		// initialize
+		Categoria category1 = insertCategory();
+		Categoria category2 = insertCategory();
+
+		// test
+		List<Categoria> categoriesFound = categoriaDao.findCategories();
+
+		assertEquals(2, categoriesFound.size());
 
 	}
 
 	@Test
 	public void testPR_UN_ED_01() {
+		// initialize
+		Categoria category = insertCategory();
+		Evento event = insertEvent(category);
+		Evento event2 = insertEvent(category);
+		Evento event3 = insertEvent(category);
+		Evento event4 = insertEvent(category);
 
+		// set some events with a future date and the others with a past date
+		event.getFecha().add(Calendar.HOUR, 1);
+		event2.getFecha().add(Calendar.HOUR, 2);
+		event3.getFecha().set(Calendar.YEAR, 2014);
+		event4.getFecha().set(Calendar.YEAR, 2015);
+
+		eventoDao.save(event);
+		eventoDao.save(event2);
+		eventoDao.save(event3);
+		eventoDao.save(event4);
+
+		// test
+		List<Evento> eventsFound = eventoDao.findEvent(category.getIdCategoria(), event.getNombreEvento(), 0, 2);
+
+		assertEquals(2, eventsFound.size());
+		assertTrue(eventsFound.contains(event));
+		assertTrue(eventsFound.contains(event2));
 	}
 
 	@Test
 	public void testPR_UN_ED_02() {
+		// initialize
+		Categoria category = insertCategory();
+		Evento event = insertEvent(category);
+		Evento event2 = insertEvent(category);
+		Evento event3 = insertEvent(category);
+		Evento event4 = insertEvent(category);
 
+		// set some events with a future date and the others with a past date
+		event.getFecha().add(Calendar.HOUR, 1);
+		event2.getFecha().add(Calendar.HOUR, 2);
+		event3.getFecha().set(Calendar.YEAR, 2014);
+		event4.getFecha().set(Calendar.YEAR, 2015);
+
+		eventoDao.save(event);
+		eventoDao.save(event2);
+		eventoDao.save(event3);
+		eventoDao.save(event4);
+
+		// test
+		List<Evento> eventsFound = eventoDao.findEvent(category.getIdCategoria(), event.getNombreEvento(), 0, 10);
+
+		assertEquals(2, eventsFound.size());
+		assertTrue(eventsFound.contains(event));
+		assertTrue(eventsFound.contains(event2));
 	}
 
 	@Test
 	public void testPR_UN_ED_03() {
+		// initialize
+		Categoria category = insertCategory();
+		Evento event = insertEvent(category);
+		Evento event2 = insertEvent(category);
+		Evento event3 = insertEvent(category);
+		Evento event4 = insertEvent(category);
 
+		// set some events with a future date and the others with a past date
+		event.getFecha().add(Calendar.HOUR, 1);
+		event2.getFecha().add(Calendar.HOUR, 2);
+		event3.getFecha().set(Calendar.YEAR, 2014);
+		event4.getFecha().set(Calendar.YEAR, 2015);
+
+		eventoDao.save(event);
+		eventoDao.save(event2);
+		eventoDao.save(event3);
+		eventoDao.save(event4);
+
+		// test
+		List<Evento> eventsFound = eventoDao.findEvent(category.getIdCategoria(), "immposibleWord", 0, 10);
+
+		assertEquals(0, eventsFound.size());
 	}
 
 	@Test
 	public void testPR_UN_ED_04() {
+
+		// intialize
+		Categoria category = insertCategory();
+		Categoria category2 = insertCategory();
+
+		Evento event = insertEvent(category);
+		Evento event2 = insertEvent(category);
+		Evento event3 = insertEvent(category);
+		Evento event4 = insertEvent(category2);
+		Evento event5 = insertEvent(category2);
+
+		event.getFecha().add(Calendar.YEAR, 1);
+		event2.getFecha().add(Calendar.YEAR, 1);
+		event3.getFecha().add(Calendar.YEAR, 1);
+		event4.getFecha().add(Calendar.YEAR, 1);
+		event5.getFecha().add(Calendar.YEAR, 1);
+
+		eventoDao.save(event);
+		eventoDao.save(event2);
+		eventoDao.save(event3);
+		eventoDao.save(event4);
+		eventoDao.save(event5);
+
+		// test
+		List<Evento> eventsFound = eventoDao.findEvent(category.getIdCategoria(), null, 0, 10);
+
+		assertEquals(3, eventsFound.size());
+		assertTrue(eventsFound.contains(event));
+		assertTrue(eventsFound.contains(event2));
+		assertTrue(eventsFound.contains(event3));
+		assertFalse(eventsFound.contains(event4));
+		assertFalse(eventsFound.contains(event5));
 
 	}
 
 	@Test
 	public void testPR_UN_ED_05() {
 
-	}
+		// initialize
+		Categoria category = insertCategory();
+		Evento event = insertEvent(category);
 
-	@Test
-	public void testPR_UN_ED_06() {
+		// test
+		boolean exists = eventoDao.findDuplicateEvents(event.getNombreEvento(), event.getFecha(),
+				category.getIdCategoria());
+
+		assertTrue(exists);
 
 	}
 
 	@Test
 	public void testPR_UN_AS_01()
 			throws InstanceNotFoundException, DuplicateInstanceException, InvalidEventDateException {
+		// initialize
 		Categoria c = new Categoria("categoria prueba");
 		c.setIdCategoria(new Long(20));
 
 		Evento evento = new Evento("eventoPrueba",Calendar.getInstance(),c);
 		evento.getFecha().add(Calendar.HOUR, 1);
 
+		// test
 		adminServiceMockito.createEvent(evento, c.getIdCategoria());
 
 
@@ -513,6 +736,7 @@ public class VVSTest {
 	public void testPR_UN_AS_02()
 			throws InstanceNotFoundException, DuplicateInstanceException, InvalidEventDateException {
 
+		// initialize
 		thrown.expect(DuplicateInstanceException.class);
 
 		Categoria categoria = new Categoria("categoriaMockito");
@@ -522,6 +746,7 @@ public class VVSTest {
 		when(eventoDaoMockito.findDuplicateEvents(evento.getNombreEvento(), evento.getFecha(),
 				categoria.getIdCategoria())).thenReturn(true);
 
+		// test
 		adminServiceMockito.createEvent(evento, categoria.getIdCategoria());
 
 		verify(eventoDaoMockito, times(1)).save(evento);

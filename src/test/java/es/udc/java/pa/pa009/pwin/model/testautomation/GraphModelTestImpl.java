09cdb2ed8189fa25fcc5e7b22135c390fa300b1d
daniel <daniel.ferreiro@udc.es>
2016-12-20 18:15:04 +0100

    Pitest y GraphWalker fallidos.

---
+++ src/test/java/es/udc/java/pa/pa009/pwin/model/testautomation/GraphModelTestImpl.java
@@ -0,0 +1,46 @@
+package es.udc.java.pa.pa009.pwin.model.testautomation;
+
+public class GraphModelTestImpl implements GraphModelTest {
+
+	@Override
+	public void Started() {
+		System.out.println("Started");
+	}
+
+	@Override
+	public void find() {
+		System.out.println("find");
+
+	}
+
+	@Override
+	public void Eliminated() {
+		System.out.println("Eliminated");
+
+	}
+
+	@Override
+	public void save() {
+		System.out.println("save");
+
+	}
+
+	@Override
+	public void initialize() {
+		System.out.println("initialize");
+
+	}
+
+	@Override
+	public void remove() {
+		System.out.println("remove");
+
+	}
+
+	@Override
+	public void Created() {
+		System.out.println("Created");
+
+	}
+
+}
